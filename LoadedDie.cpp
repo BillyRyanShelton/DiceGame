4fa6ec5e11c19b6ed07e59e223963d64aca9a33a
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-04 23:43:36 -0500

    Create LoadedDie.cpp

---
+++ LoadedDie.cpp
@@ -0,0 +1,26 @@
+/*********************************************************************
+** Author:		Billy R. Shelton
+** Date:		7/4/2018
+** Description:
+*********************************************************************/
+
+#include <iostream>
+#include "LoadedDie.hpp"
+//#include "Die.hpp"
+#include <string>
+#include <stdlib.h>		//library for random function
+#include <ctime>		//includes present time 
+
+using namespace std;
+
+int LoadedDie::roll()								//this makes the dice roll give higher outcomes on average
+{
+	loaded_outcome = (rand() % NumSides) + 3;
+
+	if (loaded_outcome > NumSides)
+	{
+		loaded_outcome = NumSides;
+	}
+	//cout << loaded_outcome << endl;
+	//cout << endl;
+	return loaded_outcome;
